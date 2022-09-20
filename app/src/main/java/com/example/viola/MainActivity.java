2ade216d1dcfb89f06b31da147ce5ed95be997c5
ViolaBusingye <violabusingye93@gmail.com>
2022-09-20 07:22:41 -0700

    Viola App

---
+++ app/src/main/java/com/example/viola/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.viola;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
